b1e1f25f0df5b5c4a8f0c157e7c76957e1ef97c8
Brian Best <brian.brianb64@gmail.com>
2025-07-28 16:16:31 -0400

    Remove legacy files and data for the FFX Monsters project, including SQL scripts, backend, native app components, and web assets.

--- ffx-backend/src/routes/locations.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationsController = require('../controllers/locations');
-
-router.get('/', locationsController.getAllLocations);
-
-module.exports = router; 
--- ffx-native/src/api.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'http://192.168.86.38:3000/api'; 
